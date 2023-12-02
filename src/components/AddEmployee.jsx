4a4346891c13e9a74987431512f77d213e397752
Marouf <maroufibrahimi7264@gmail.com>
2023-12-02 19:31:54 +0430

    add employee form

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default class AddEmployee extends Component {
   constructor(props) {
@@ -9,54 +10,95 @@ export default class AddEmployee extends Component {
       lastName: "",
       emailId: "",
     };
+
+    this.changeFirstName = this.changeFirstName.bind(this);
+    this.changeLastName = this.changeLastName.bind(this);
+    this.changeEmailId = this.changeEmailId.bind(this);
+    this.saveEmployee = this.saveEmployee.bind(this);
   }
+
+  saveEmployee = (e) => {
+    e.preventDefault();
+    let employee = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      emailId: this.state.emailId,
+    };
+    console.log(JSON.stringify(employee));
+  };
 
   changeFirstName(event) {
     this.setState({ firstName: event.target.value });
   }
 
+  changeLastName(event) {
+    this.setState({ lastName: event.target.value });
+  }
+
+  changeEmailId(event) {
+    this.setState({ emailId: event.target.value });
+  }
+
+  cancel() {
+    this.props.useNavigate("/employees");
+  }
+
   render() {
     return (
       <div>
-        <div className="container">
+        <div className="container mt-3">
           <div className="row">
             <div className="card col-md-6 offset-md-3 offset-md-3">
               <h3 className="text-center">Add Employee</h3>
               <div className="card-body">
                 <form>
-                  <div className="form-group">
+                  <div className="form-group mt-3">
                     <label>First Name</label>
                     <input
                       type="text"
+                      className="form-control"
                       placeholder="First Name"
                       name="firstname"
-                      className="form-control"
                       value={this.state.firstName}
                       onChange={this.changeFirstName}
                     />
                   </div>
-                  <div className="form-group">
+                  <div className="form-group mt-3">
                     <label>Last Name</label>
                     <input
                       type="text"
+                      className="form-control"
                       placeholder="Last Name"
                       name="lastname"
-                      className="form-control"
                       value={this.state.lastName}
                       onChange={this.changeLastName}
                     />
                   </div>
-                  <div className="form-group">
-                    <label>Email ID</label>
+                  <div className="form-group mt-3">
+                    <label>Email Address</label>
                     <input
                       type="email"
-                      placeholder="Email ID"
-                      name="email"
                       className="form-control"
+                      placeholder="Email Address"
+                      name="email"
                       value={this.state.emailId}
                       onChange={this.changeEmailId}
                     />
                   </div>
+
+                  <button
+                    className="btn btn-success mt-3"
+                    onClick={this.saveEmployee}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="btn btn-danger mt-3"
+                    onClick={this.cancel.bind(this)}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Cancel
+                  </button>
                 </form>
               </div>
             </div>
