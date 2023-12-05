acd37368ea3e3552bf12799e3e327e5a630097cb
Marouf <maroufibrahimi7264@gmail.com>
2023-12-05 14:50:05 +0430

    another version of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import ListEmployee from "./components/ListEmployee";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddEmployee from "./components/AddEmployee";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <div className="container">
-        <Routes>
-          <Route path="/" element={<ListEmployee />} />
-          <Route path="/employees" element={<ListEmployee />} />
-          <Route path="/add-employee" element={<AddEmployee />} />
-        </Routes>
+        <Switch>
+          <Route path="/" exact component={ListEmployee}></Route>
+          <Route path="/employees" component={ListEmployee}></Route>
+          <Route path="/add-employee" component={AddEmployee}></Route>
+          <Route path="/update-employee/:id" component={UpdateEmployee}></Route>
+        </Switch>
       </div>
       <Footer />
     </Router>
